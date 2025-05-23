56741ead752673e90e07ba16d63c4fd682296675
Gissgarcia <giss.garcia@duocuc.cl>
2025-05-22 21:10:58 -0400

    Se añadio un comentario a Modelo de producto

--- Ecomarket/producto/producto/src/main/java/com/ecomarketspa/producto/model/ProductoModel.java
+++ Ecomarket/producto/producto/src/main/java/com/ecomarketspa/producto/model/ProductoModel.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+// este es un cambio
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
